3fe0cf2210ad99a5cef4f8c6fecc0d3ebb7cfad3
Yt1m <144239923+Yt1m@users.noreply.github.com>
2023-12-26 12:24:37 +0200

    Add files via upload

---
+++ 5.2-Высокий.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace ReplaceStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "abc*def*ghi*jkl*";
+            StringBuilder result = new StringBuilder(input);
+
+            int count = 1;
+            int index = 0;
+            while ((index = result.ToString().IndexOf("*", index)) != -1)
+            {
+                result.Replace("*", count.ToString(), index, 1);
+                count++;
+                index++;
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
