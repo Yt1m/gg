96742c3939ad441e8331509b1044a7bfac6f522a
Yt1m <144239923+Yt1m@users.noreply.github.com>
2023-12-27 21:53:44 +0200

    Add files via upload

---
+++ 3.4-базоый.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double start = 1.1;
+        double end = 2.9; 
+        double step = 0.2;
+
+        Console.WriteLine("x\ty");
+        for (double x = start; x <= end; x += step)
+        {
+            double y = Math.Pow(x, 3) - 6 * Math.Pow(x, 2) + 2;
+            Console.WriteLine($"{x}\t{y}");
+        }
+
+        Console.ReadLine();
+    }
+}
