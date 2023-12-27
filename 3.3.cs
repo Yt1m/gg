3e59bd90b87438ce4f07605603b6e980fc3d4d56
Yt1m <144239923+Yt1m@users.noreply.github.com>
2023-12-27 11:25:10 +0200

    Add files via upload

---
+++ 3.3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static double CalculateSeriesSum(double x, int n)
+    {
+        double sum = 1;
+        double term = 1;
+        double factorial = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            term *= (2 * x) / i; 
+            sum += term; 
+            factorial *= i;
+        }
+        return sum;
+    }
+    static void Main(string[] args)
+    {
+        double x = 1.5;
+        int n = 10; 
+
+        double sum = CalculateSeriesSum(x, n);
+
+        Console.WriteLine("Сумма первых {0} членов ряда: {1}", n, sum);
+    }
+}
