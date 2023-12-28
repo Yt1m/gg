3893d5d13db2376a3899d2704b0c3bce9a234c70
Yt1m <144239923+Yt1m@users.noreply.github.com>
2023-12-28 18:54:27 +0200

    Add files via upload

---
+++ 6.2-Базовый на 4.cs
@@ -0,0 +1,55 @@
+﻿using MatrixLibrary;
+using System;
+
+namespace MatrixLibrary
+{
+    public class MatrixOperations
+    {
+        public static int CalculateSumInRange(int[,] matrix, int n, int m)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    int element = matrix[i, j];
+                    if (element >= -2 && element <= 3)
+                    {
+                        sum += element;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк матрицы:");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцов матрицы:");
+            int m = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, m];
+
+            Console.WriteLine($"Введите элементы матрицы {n}x{m}:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int sum = MatrixOperations.CalculateSumInRange(matrix, n, m);
+            Console.WriteLine($"Сумма элементов матрицы, находящихся в интервале [-2;3]: {sum}");
+
+            Console.ReadLine();
+        }
+    }
+}
